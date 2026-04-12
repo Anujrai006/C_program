e47cb6ecf80c12aaacea89e7ec557d2856ce4447
Anuj Rai <anujxilling253745@gmail.com>
2026-04-12 13:28:21 +0545

    file created

---
+++ star.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    for(int a=0;a<5;a++)
+    {
+        for(int b=0;b<=a;b++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+
+    }
+}
