bdb6c43607511bb925f582e62c3996d835a8477b
Anuj Rai <anujxilling253745@gmail.com>
2026-04-13 10:44:31 +0545

    new witch question done

---
+++ switch1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main()
+{
+    int choice;
+    printf("Enter 1 for sunday, 2 for monday...");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+        printf("Sunday");
+        break;
+        case 2:
+        printf("Monday");
+        break;
+        case 3:
+        printf("Tuesday");
+        break;
+        case 4:
+        printf("Wednesday");
+        break;
+        case 5:
+        printf("Thursday");
+        break;
+        case 6:
+        printf("Friday");
+        break;
+        case 7:
+        printf("Saturday");
+        break;
+        default:
+          printf("Invalid Option");
+
+    }
+}
