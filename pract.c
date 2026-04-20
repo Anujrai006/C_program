e5a9c28c26743a412d1a21ede8615395d74306d9
Anuj Rai <anujxilling253745@gmail.com>
2026-04-20 20:44:45 +0545

    new qn

--- pract.c
+++ pract.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main()
+void main()
 {
-   int arr1[5];
-   int arr2[];
+   for(int i=1;i<=10;i++)
+   {
+      printf("Table of %d\n",i);
+         for(int a=1;a<=10;a++)
+         {
+            printf("%d * %d : %d\n",a,i,a*i);
+         }
+         printf("\n");
+
+   }
 }
